862d2670074288600b043878238b7c0b50734805
Justus Rijke <justus@industrialbrains.nl>
2022-12-19 17:36:42 +0100

    refactored SystemBase states & removed obsolete vars & PowerOff cmd

--- Tests/test_system.py
+++ Tests/test_system.py
@@ -3,7 +3,7 @@
 import sys
 import unittest
 
-from connection import cold_reset, conn, wait_cycles
+from connection import cold_reset, conn, wait_cycles, wait_value
 
 COLD_RESET = False
 COLD_RESET = True
@@ -12,6 +12,8 @@ COLD_RESET = True
 class TestSystem(unittest.TestCase):
 
     PREFIX = "SystemBase"
+    PREFIX_MODULE = "PRG_TEST.fbDummyModule"
+    PREFIX_HMI = "GVL_HMI.fbHMIControl"
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -36,34 +38,38 @@ class TestSystem(unittest.TestCase):
 
     def _initalize(self):
         """After cold reset, system ends up in state `Safe`"""
-        wait_cycles(7)  # it takes 7 cycles to initialize (tested with breakpoint)
-        self.assertEqual(
-            conn.read_by_name(f"{self.PREFIX}.fbFaultHandler.nActiveFaults"),
-            0,
-        )
+        conn.write_by_name(f"{self.PREFIX_MODULE}.bAllowInit", True)
 
     def _enable(self):
         """Move from state `Safe` to state `Enable`"""
         self._initalize()
+        conn.write_by_name(f"{self.PREFIX_MODULE}.bAllowIsSafe", True)
+        conn.write_by_name(f"{self.PREFIX_MODULE}.bAllowReset", True)
         conn.write_by_name(f"{self.PREFIX}.ibEstopOk", True)
-        conn.write_by_name(f"{self.PREFIX}.ibSystemSafetyOk", True)
+        # Reduce air pressure sensor delay (default 1sec)
+        conn.write_by_name(f"{self.PREFIX}.fbAirpressureOk.tDelay", 0)
         conn.write_by_name(f"{self.PREFIX}.fbAirpressureOk.Istatus", True)
-        wait_cycles(150)  # a whole lot of cycles! why?
+        wait_cycles(50) # TODO: why so many cycles?
+        self._trigger_falling_edge(f"{self.PREFIX}.fbCmdReset.Istatus")
+        self.assertTrue(wait_value(f"{self.PREFIX}.bEnabled", True, 1))
 
     def _idle(self):
         """Move from state `Enable` to state `Idle`. Moves through state `Home`"""
         self._enable()
+        conn.write_by_name(f"{self.PREFIX_MODULE}.bAllowHome", True)
         self._trigger_falling_edge(f"{self.PREFIX}.fbCmdStartSystem.Istatus")
 
     def _semiauto(self):
         """Move from state `Idle` to state `SemiAuto`"""
         self._idle()
+        conn.write_by_name(f"{self.PREFIX_MODULE}.bAllowSemiAuto", True)
         self._trigger_falling_edge(f"{self.PREFIX}.fbCmdStartSystem.Istatus")
 
     def _automatic(self):
         """Move from state `SemiAuto` to state `Automatic`
         At least one module needs to be registered, because of the inverted bAutomatic feedback."""
         self._semiauto()
+        conn.write_by_name(f"{self.PREFIX_MODULE}.bAllowAutomatic", True)
         self._trigger_falling_edge(f"{self.PREFIX}.fbCmdStartAuto.Istatus")
 
     def test_01_initialize(self):
@@ -72,7 +78,6 @@ class TestSystem(unittest.TestCase):
 
     def test_02_enable(self):
         self._enable()
-        self.assertTrue(conn.read_by_name(f"{self.PREFIX}.bEnabled"))
 
     def test_03_idle(self):
         self._idle()
@@ -85,6 +90,17 @@ class TestSystem(unittest.TestCase):
     def test_05_automatic(self):
         self._automatic()
         self.assertTrue(conn.read_by_name(f"{self.PREFIX}.bAutomatic"))
+
+    def test_06_manual(self):
+        self._enable()
+        conn.write_by_name(f"{self.PREFIX_MODULE}.bAllowManual", True)
+        conn.write_by_name(f"{self.PREFIX_HMI}.bInScreenManual", True)
+        self.assertTrue(conn.read_by_name(f"{self.PREFIX}.bManual"))
+
+    def test_07_fault(self):
+        self._enable()
+        conn.write_by_name(f"{self.PREFIX}.ibEstopOk", False)
+        self.assertTrue(wait_value(f"{self.PREFIX}.bEnabled", False, 1))
 
 
 if __name__ == "__main__":
